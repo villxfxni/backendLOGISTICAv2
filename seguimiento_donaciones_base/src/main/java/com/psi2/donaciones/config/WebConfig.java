2c947e97369f222bfe5d91721181e8c1892fac39
Ana Villafani <146146466+villxfxni@users.noreply.github.com>
2025-10-29 10:12:10 -0400

    Update WebConfig.java

--- seguimiento_donaciones_base/src/main/java/com/psi2/donaciones/config/WebConfig.java
+++ seguimiento_donaciones_base/src/main/java/com/psi2/donaciones/config/WebConfig.java
@@ -12,12 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-<<<<<<< HEAD
         .allowedOrigins("http://localhost:3000")
         //.allowedOrigins("http://192.168.0.18:3000")
-=======
-        .allowedOrigins("http://192.168.0.18:3000")
->>>>>>> f46334bb078ae149261cab1d5b6961faef426b06
+
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With");
     }
